12a9b5fbc7a5ecd28757253bc69d7513f07b7e48
noshenko98 <noshenko98@gmail.com>
2026-06-23 08:56:31 +0300

    Modification models and settings

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,10 +5,12 @@ class Tag(models.Model):
     name = models.CharField(max_length=100)
 
 
-
 class Task(models.Model):
     content = models.TextField()
     datetime = models.DateTimeField(auto_now_add=True)
     expiration_datetime = models.DateTimeField(null=True, blank=True)
     status = models.BooleanField(default=False)
-    tags = models.ManyToManyField(Tag, related_name="tasks", on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ("status", "datetime")
