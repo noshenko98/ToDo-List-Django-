1bde67402828b44dc45b647bf6e7770d8fd47a69
noshenko98 <noshenko98@gmail.com>
2026-07-04 15:04:45 +0300

    Registration for the admin panel

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from task_manager.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["id", "content", "status",
+                    "datetime", "expiration_datetime" ]
+    list_filter = ["status", "tags"]
+    search_fields = ["content"]
+    list_per_page = 10
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    search_fields = ["name"]
+    list_per_page = 10
+
+admin.site.unregister(Group)
